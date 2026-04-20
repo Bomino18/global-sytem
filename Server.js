abfc14e4802f8e9f52521f22a301141a53d6aa4d
Bomino18 <b7772011@gmail.com>
2026-04-20 15:29:34 +0300

    Fix import statement for express in Server.js
    global-investment

---
+++ Server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("🚀 Server is working");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
